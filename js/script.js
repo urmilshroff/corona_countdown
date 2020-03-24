a8eab1f1354c155c8eeab7c03355f14980002e27
urmilshroff <urmilshroff@gmail.com>
2020-03-24 21:19:20 +0530

    Created website

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var end = new Date("April 15, 2020 00:00:00").getTime();
+
+var countdown = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = end - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(countdown);
+    }
+
+}, 1000);
